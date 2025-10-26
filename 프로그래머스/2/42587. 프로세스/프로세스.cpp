7bcd35bc8880b85ba24cdc7e7fc1ddee93746d1c
currentnine <116486410+currentnine@users.noreply.github.com>
2025-10-26 15:30:29 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,35 +1,30 @@
 #include <string>
 #include <vector>
 #include <queue>
-
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<pair<int, int>> q;
     priority_queue<int> pq;
-    queue<pair<int, int> >q;
-    int answer = 1;
-    for(int i = 0; i< priorities.size(); i++){
+    for(int i = 0; i < priorities.size(); i++){
         q.push({i, priorities[i]});
         pq.push(priorities[i]);
     }
     int count = 1;
-    
     while(1){
-        pair<int, int> temp  = q.front();
-        q.pop();
-        if(temp.second != pq.top())
-            q.push(temp);
-        else
-            if(temp.first == location){
+        pair<int, int> p = q.front(); q.pop();
+        if(p.second != pq.top()) q.push(p);
+        else{
+            if(p.first == location){
                 answer = count;
                 break;
             }
-        else{
-            count++;
-            pq.pop();
+            else{
+                count++;
+                pq.pop();
+            }
         }
     }
-    
     return answer;
-    
 }
